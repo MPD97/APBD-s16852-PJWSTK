6bbea08f17a0483b042b17d1ba9e877569468557
Mateusz <mateusz@o2.pl>
2020-03-02 13:08:18 +0100

    Zadanie 1, 2, 3 i 4 w polowie

--- zjazd-1/Cw1/Cw1/Program.cs
+++ zjazd-1/Cw1/Cw1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (string.IsNullOrEmpty(args[0]))
+            {
+                throw new ArgumentNullException("Nie podano argumentu");
+            }
+            if (new Regex(@"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$", RegexOptions.IgnoreCase).Match(args[0]).Success == false)
+            {
+                throw new ArgumentException("Podany adres url jest niepoparwny");
+            }
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage result;
+                try
+                {
+                    result = await client.GetAsync(args[0]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Wystąpił bład podczas ładowania strony.");
+                    return;
+                }
+                if (result.IsSuccessStatusCode)
+                {
+                    var emailPattern = "[a-z0-9]+@[a-z0-9]+\\.[a-z]+";
+                    var regex = new Regex(emailPattern, RegexOptions.IgnoreCase);
+
+                    var pageContent = await result.Content.ReadAsStringAsync();
+
+                    MatchCollection matches = regex.Matches(pageContent);
+                    Console.WriteLine($"Znaleziono: {matches.Count} adresy/ów email.");
+
+                    for (int i = 0; i < matches.Count; i++)
+                    {
+                        Console.WriteLine($"{i}. {matches[i]}");
+                    }
+                }
+            }
+            Console.ReadKey();
+
         }
     }
 }
