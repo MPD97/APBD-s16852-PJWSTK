5dfe9ef8f806e994a4de7b48172e40eb6b345447
mpd97 <mateuszpracadom@gmail.com>
2020-03-16 19:57:55 +0100

    Properties and enum

--- zjazd-2/Cw1/Cw1/Program.cs
+++ zjazd-2/Cw1/Cw1/Program.cs
@@ -4,9 +4,16 @@ namespace Cw1
 {
     class Program
     {
+        public static string PathCSV { get; set; } = "data.csv";
+        public static string ResultPath { get; set; } = "żesult.xml";
+        public static OutputFormat OutputFormat { get; set; } = OutputFormat.XML;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
+    }
+    public enum OutputFormat
+    {
+        XML
     }
 }
