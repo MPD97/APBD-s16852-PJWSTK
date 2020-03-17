2cd20a95a214f3e0d9eb20cb05d7241ecf25040b
Mateusz <mateusz@o2.pl>
2020-03-17 09:14:06 +0100

    Naming refactor

--- zjazd-2/Cw1/Cw1/Program.cs
+++ zjazd-2/Cw1/Cw1/Program.cs
@@ -2,16 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace Cw1
 {
     class Program
     {
-        private static string _currentDirectory = Directory.GetCurrentDirectory();
+        private static readonly string CurrentDirectory = Directory.GetCurrentDirectory();
 
-        private static string _pathCSV { get; set; } = Path.Combine(_currentDirectory, "data.csv");
-        private static string _resultPath { get; set; } = Path.Combine(_currentDirectory, "żesult.xml");
-        private static OutputFormat _outputFormat { get; set; } = OutputFormat.XML;
+        private static string PathCsv { get; set; } = Path.Combine(CurrentDirectory, "data.csv");
+        private static string ResultPath { get; set; } = Path.Combine(CurrentDirectory, "żesult.xml");
+        private static OutputFormat OutputFormat { get; set; } = OutputFormat.XML;
 
         static void Main(string[] args)
         {
@@ -22,14 +23,19 @@ namespace Cw1
             if (args.Length >= 1 && string.IsNullOrEmpty(args[0]) == false)
             {
                 ValidatePossiblePathToFile(args[0]);
-                var combinedPath = Path.Combine(_currentDirectory, args[0]);
-                _pathCSV = args[0];
+                var combinedPath = Path.Combine(CurrentDirectory, args[0]);
+                PathCsv = args[0];
             }
 
             if (args.Length >= 2 && string.IsNullOrEmpty(args[1]) == false)
             {
+                if (ResultPath.Contains('.'))
+                {
+                    var indexOfDot = ResultPath.LastIndexOf('.');
+                    ResultPath = ResultPath.Substring(indexOfDot, ResultPath.Length - indexOfDot);
+                }
                 ValidatePossiblePathToFile(args[1]);
-                _resultPath = args[1];
+                ResultPath = args[1];
             }
 
             if (args.Length >= 3 && string.IsNullOrEmpty(args[2]) == false)
@@ -38,21 +44,32 @@ namespace Cw1
                 Enum.TryParse(typeof(OutputFormat), args[2], out output);
                 if ((OutputFormat) output == OutputFormat.NULL)
                 {
-                    Log.Logger.Error($"Nieznany typ pliku wyjściowego. Używam formatu domyślnego: {_outputFormat}");
+                    Log.Logger.Error($"Nieznany typ pliku wyjściowego. Używam formatu domyślnego: {OutputFormat}");
                 }
-                _outputFormat = (OutputFormat) output;
+                OutputFormat = (OutputFormat) output;
             }
             List<Student> students;
 
-            var parser = new StudentParser(_pathCSV, _resultPath, _outputFormat);
+            var parser = new StudentParser(PathCsv, ResultPath, OutputFormat);
             if ((students = parser.TryLoadInputFile()) == null)
             {
-                Log.Logger.Error($"Wynik przetwarzania pliku wejściowego to null.");
+                var message = "Wynik przetwarzania pliku wejściowego to null.";
+                Log.Logger.Error(message);
+                throw new ArgumentNullException(message);
             }
             var college = new Uczelnia();
             college.Author = "Mateusz Ambroziak";
             college.CreatedAt = DateTime.Now.ToString("dd.MM.yyyy");
+            college.Students = students;
 
+
+            
+            XmlSerializer serializer = new XmlSerializer(typeof(Uczelnia));
+
+            using (StreamWriter streamWriter = new StreamWriter(ResultPath))
+            {
+                
+            }
 
             Console.ReadLine();
         }
@@ -63,7 +80,7 @@ namespace Cw1
             {
                 InputError();
             }
-            if (Directory.Exists(Path.Combine(_currentDirectory, file)))
+            if (Directory.Exists(Path.Combine(CurrentDirectory, file)))
             {
                 InputError();
             }
