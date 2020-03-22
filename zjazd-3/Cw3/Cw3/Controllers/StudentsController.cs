038965a037288803d829ed69b4d5e9065cc66dd5
mpd97 <mateuszpracadom@gmail.com>
2020-03-22 17:04:25 +0100

    Injecting IDbService to the StudentsController via constructor

--- zjazd-3/Cw3/Cw3/Controllers/StudentsController.cs
+++ zjazd-3/Cw3/Cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3.DAL;
 using Cw3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +13,17 @@ namespace Cw3.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
-        public string GetStudent(string orderBy)
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
-            return $"Kowalski, Maleski, Andrzejewski sortowanie={orderBy}";
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
         }
         [HttpGet("{id}")]
         public IActionResult GetStudent(int id)
