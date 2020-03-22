ba3710fe7d5ae00b2ef84256443a3e742f6f9b9e
mpd97 <mateuszpracadom@gmail.com>
2020-03-22 16:56:36 +0100

    Changed property into method

--- zjazd-3/Cw3/Cw3/DAL/IDbService.cs
+++ zjazd-3/Cw3/Cw3/DAL/IDbService.cs
@@ -8,6 +8,6 @@ namespace Cw3.DAL
 {
     public interface IDbService
     {
-        public IEnumerable<Student> GetStudents { get; set; }
+        public IEnumerable<Student> GetStudents();
     }
 }
