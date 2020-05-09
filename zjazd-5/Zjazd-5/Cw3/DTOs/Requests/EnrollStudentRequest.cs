c3c76952b165a8a9cccc228ea684fd2260fd6435
mpd97 <mateuszpracadom@gmail.com>
2020-05-09 15:10:40 +0200

    Created directories and cleaned up project

---
+++ zjazd-5/Zjazd-5/Cw3/DTOs/Requests/EnrollStudentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw4.DTOs.Requests
+{
+    public class EnrollStudent
+    {
+    }
+}
