b6f55cf199c2aea02d0b9bc62373c96990256b94
Mateusz <mateusz@o2.pl>
2020-05-25 13:08:15 +0200

    Init with old project

---
+++ zjazd-7/Cw3/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw4.Middlewares;
+using Cw4.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Cw3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IStudentDbService, SqlServerStudentDbService>();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IStudentDbService dbService)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseMiddleware<LoggingMiddleware>();
+
+            app.Use(async (context, next) =>
+            {
+                context.Request.Headers.TryGetValue("Index", out var index);
+
+                if (string.IsNullOrWhiteSpace(index) || string.IsNullOrEmpty(index))
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    await context.Response.WriteAsync("Nie podano indeksu");
+                    return;
+                }
+
+                if (dbService.StudentExist(index) == false)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    await context.Response.WriteAsync("Brak dostępu.");
+                    return;
+                }
+
+                await next();
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
