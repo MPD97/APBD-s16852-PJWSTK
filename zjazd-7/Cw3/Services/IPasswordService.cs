9b021a3c1f06b847a4adba23a8afdf684679dec5
MPD97 <mateuszpracadom@gmail.com>
2020-05-27 11:38:33 +0200

    Added IpasswordService

---
+++ zjazd-7/Cw3/Services/IPasswordService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw4.Services
+{
+    public interface IPasswordService
+    {
+        public string Create(string value, string salt);
+
+        public bool Validate(string value, string salt, string hash);
+
+        public string CreateSalt();
+    }
+    public class PBKDFPasswordService : IPasswordService
+    {
+        public string Create(string value, string salt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateSalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(string value, string salt, string hash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
