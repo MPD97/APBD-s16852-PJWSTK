acc7af3f510b898b04041b27ff7d41bdafe5776a
Mateusz <mateusz@o2.pl>
2020-06-01 11:16:59 +0200

    Patient

--- zjazd-11/zjazd-11/zjazd-11/Models/Prescription.cs
+++ zjazd-11/zjazd-11/zjazd-11/Models/Prescription.cs
@@ -1,4 +1,6 @@
-﻿namespace zjazd_11.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace zjazd_11.Models
 {
     public class Prescription
     {
@@ -6,5 +8,16 @@
 
         public decimal Date { get; set; }
         public decimal DueDate { get; set; }
+    }
+
+    public class Patient
+    {
+        public int PatientId { get; set; }
+
+        [MaxLength(100)]
+        public decimal FirstName { get; set; }
+
+        [MaxLength(100)]
+        public decimal LastName { get; set; }
     }
 }
