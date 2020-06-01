56fc5ffe5de560d5e7dd15cb4d29c82cd651574a
Mateusz <mateusz@o2.pl>
2020-06-01 10:07:02 +0200

    add EnrollStudent in efc

--- zjazd-10/Cw3/Controllers/EnrollmentsController.cs
+++ zjazd-10/Cw3/Controllers/EnrollmentsController.cs
@@ -11,6 +11,7 @@ using Cw4.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cw4.Controllers
 {
@@ -18,16 +19,17 @@ namespace Cw4.Controllers
     [ApiController]
     public class EnrollmentsController : ControllerBase
     {
-        private IStudentDbService _service;
 
-        public EnrollmentsController(IStudentDbService service)
+        public s16852Context Context { get; set; }
+
+        public EnrollmentsController(s16852Context context)
         {
-            _service = service;
+            Context = context;
         }
 
         [HttpPost]
         [Authorize(Roles = "employee")]
-        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        public async Task<IActionResult> EnrollStudent(EnrollStudentRequest request)
         {
             if (ModelState.IsValid == false)
             {
@@ -36,13 +38,47 @@ namespace Cw4.Controllers
                 return BadRequest(allErrors);
             }
 
-            var result = _service.EnrollStudent(request);
-            if (result.Success == false)
+            Studies studies = await Context.Studies.FirstOrDefaultAsync(stud => stud.Name == request.Studies);
+            if (studies == null)
             {
-                return BadRequest(result.Message);
+                return BadRequest("Studia nie istnieją");
             }
 
-            return StatusCode(201, result.Model);
+            Enrollment enrollment = await Context.Enrollment.FirstOrDefaultAsync(en => en.IdStudy == studies.IdStudy && en.Semester == 1);
+            if (enrollment == null)
+            {
+                enrollment = new Enrollment { Semester = 1, IdStudy = studies.IdStudy, StartDate = DateTime.Now };
+                Context.Add(enrollment);
+            }
+
+            Student student = await Context.Student.FirstOrDefaultAsync(s => s.IndexNumber == request.IndexNumber);
+            if (studies != null)
+            {
+                return BadRequest("Student o takim indeksie już istnieje w bazie danych.");
+            }
+            student = new Student
+            {
+                IndexNumber = request.IndexNumber,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                BirthDate = request.Birthdate,
+                IdEnrollment = enrollment.IdEnrollment
+            };
+            Context.Add(student);
+
+            if (await Context.SaveChangesAsync() == 0)
+            {
+                return StatusCode(500, "Błąd podczas zapisu danych do bazy.");
+            }
+            EnrollStudentResponse result = new EnrollStudentResponse
+            {
+                IndexNumber = student.IndexNumber,
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                Semester = enrollment.Semester,
+                StartDate = enrollment.StartDate,
+            };
+            return StatusCode(201, result);
         }
 
         [HttpPost("promotions")]
