612d0954c1cd0e5c7150183e12169c618cfb1c36
Mateusz <mateusz@o2.pl>
2020-06-01 09:12:45 +0200

    Get student by index number

--- zjazd-10/Cw3/Controllers/StudentsController.cs
+++ zjazd-10/Cw3/Controllers/StudentsController.cs
@@ -31,39 +31,7 @@ namespace Cw3.Controllers
         [HttpGet("{indexNumber}")]
         public IActionResult GetStudent(string indexNumber)
         {
-            Student student = new Student();
-
-            using (var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s16852;Integrated Security=True"))
-            {
-                using (SqlCommand command = new SqlCommand())
-                {
-
-                    command.Connection = connection;
-                    command.CommandText = @"SELECT TOP (1) [FirstName]
-                                                  ,[LastName]
-                                                  ,[BirthDate]
-	                                              ,[Name]
-	                                              ,[Semester]
-                                              FROM Student
-                                              INNER JOIN Enrollment
-                                              ON Student.IdEnrollment = Enrollment.IdEnrollment
-                                              Inner Join Studies
-                                              ON Enrollment.IdStudy = Studies.IdStudy
-                                              WHERE Student.IndexNumber = @indexNumber;";
-                    command.Parameters.AddWithValue("indexNumber", indexNumber);
-                    connection.Open();
-                    var dataReader = command.ExecuteReader();
-                    while (dataReader.Read())
-                    {
-                        student.FirstName = dataReader["FirstName"].ToString();
-                        student.LastName = dataReader["LastName"].ToString();
-                        student.BirthDate = DateTime.Parse(dataReader["BirthDate"].ToString());
-                        student.StudiesName = dataReader["Name"].ToString();
-                        student.SemestrNumber = int.Parse(dataReader["Semester"].ToString());
-                    }
-                }
-            }
-            return Ok(student);
+            return Ok(Context.Student.FirstOrDefaultAsync(student => student.IndexNumber == indexNumber));
         }
 
         [HttpPost]
